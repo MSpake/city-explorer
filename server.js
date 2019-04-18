e0f66b54c82b8d26aaff87b4ce2c2a497097d172
mspake <morgana.spake@gmail.com>
2019-04-17 22:51:06 -0700

    server issues

--- server.js
+++ server.js
@@ -14,6 +14,15 @@ const PORT = process.env.PORT || 3000;
 const express = require('express');
 const cors = require('cors');
 const superagent = require('superagent');
+const pg = require('pg');
+
+//==========================================
+// Postgres Client Setup
+//==========================================
+
+const client = pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', error => console.error(error));
 
 //==========================================
 // Server Definition
@@ -22,39 +31,47 @@ const superagent = require('superagent');
 const app = express();
 app.use(cors());
 
+
 //==========================================
-// Server
+// Constructors
 //==========================================
 
-//the route
-//request = data from query. example, from a front end query
-//can test in localhost:3000/location to verify
+function Location(query, data) {
+  this.search_query = query;
+  this.formatted_query = data.formatted_address;
+  this.latitude = data.geometry.location.lat;
+  this.longitude = data.geometry.location.lng;
+}
 
-// Switched app.get from an anonymous function to a named callback.
-app.get('/location', searchLatLng);
+function DailyWeather(forecast, time) {
+  this.forecast = forecast;
+  this.time = new Date(time * 1000).toString().slice(0, 15);
+}
 
-app.get('/weather', searchWeather);
-
-// Standard response for when a route that does not exist is accessed.
-app.use('*', (request, response) => {
-  response.send('Our server runs.');
-})
 
 //==========================================
 // Helper Functions
 //==========================================
 
 function searchLatLng(request, response) {
-  // take the data from the front end, as the searched for location ('berlin')
+  // take the data from the front end
   const query = request.query.data;
   const geocodeData = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
-
-  superagent.get(geocodeData).then(locationResult => {
-    const first = locationResult.body.results[0];
-    const responseObject = new Location(query, first);
-    response.send(responseObject);
-  })
-
+  console.log('checking the DATABASE');
+  client.query('SELECT * FROM locations WHERE search_query=$1', [query])
+    .then(result => {
+      console.log('result from DATABASE');
+      if (result.rows.length) { // (stuff in the db)
+        console.log('Exists in the DATABASE');
+        response.send(result.rows[0]);
+      } else {
+        superagent.get(geocodeData).then(locationResult => {
+          const first = locationResult.body.results[0];
+          const responseObject = new Location(query, first);
+          response.send(responseObject);
+        });
+      }
+    });
 }
 
 function searchWeather(request, response) {
@@ -64,29 +81,26 @@ function searchWeather(request, response) {
   superagent.get(weatherData).then(weatherResult => {
     const weeklyWeatherArray = weatherResult.body.daily.data.map(dayObj => new DailyWeather(dayObj.summary, dayObj.time));
     response.send(weeklyWeatherArray);
-  })
+  });
 }
 
 
 //==========================================
-// Constructors
+// Server
 //==========================================
 
-function DailyWeather(forecast, time) {
-  this.forecast = forecast;
-  this.time = new Date(time * 1000).toString().slice(0, 15);
-}
+app.get('/location', searchLatLng);
 
-function Location(query, data) {
-  this.search_query = query;
-  this.formatted_query = data.formatted_address;
-  this.latitude = data.geometry.location.lat;
-  this.longitude = data.geometry.location.lng;
-}
+app.get('/weather', searchWeather);
+
+// Standard response for when a route that does not exist is accessed.
+app.use('*', (request, response) => {
+  response.send('Route not available');
+});
 
 //==========================================
 
 //server start
 app.listen(PORT, () => {
-  console.log(`app is up on PORT ${PORT}`)
-})
+  console.log(`app is up on PORT ${PORT}`);
+});
