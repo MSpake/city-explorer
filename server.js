55d35335e0cc69e318b73e1a17d1d1d39a0fad84
mspake <morgana.spake@gmail.com>
2019-04-21 21:27:47 -0700

    weather database interaction working

--- server.js
+++ server.js
@@ -32,6 +32,16 @@ const app = express();
 app.use(cors());
 
 //==========================================
+// SQL
+//==========================================
+
+const SQL = {};
+SQL.getLocation = 'SELECT * FROM locations WHERE search_query=$1';
+SQL.insertLocation = 'INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4)';
+SQL.getLocationReference = 'SELECT id FROM locations WHERE latitude=$1 AND longitude=$2';
+// SQL.getData = `SELECT * FROM ${route} WHERE location_id=${locationId}`;
+
+//==========================================
 // Constructors
 //==========================================
 
@@ -56,7 +66,7 @@ function locationQuery(request, response) {
   console.log('location');
 
   const query = request.query.data;
-  client.query('SELECT * FROM locations WHERE search_query=$1', [query])
+  client.query(SQL.getLocation, [query])
     .then(result => {
       if (result.rows.length) {
         console.log('from database');
@@ -66,22 +76,7 @@ function locationQuery(request, response) {
         searchLocation(query, response);
       }
     });
-
-
 }
-
-function weatherQuery(request, response) {
-  console.log('weather');
-
-  const query = request.query.data;
-  const weatherData = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${query.latitude},${query.longitude}`;
-
-  superagent.get(weatherData).then(result => {
-    const weeklyWeatherArray = result.body.daily.data.map(dayObj => new DailyWeather(dayObj.summary, dayObj.time));
-    response.send(weeklyWeatherArray);
-  });
-}
-
 
 function searchLocation(query, response) {
   const geocodeData = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
@@ -89,11 +84,43 @@ function searchLocation(query, response) {
   superagent.get(geocodeData).then(result => {
     const firstResult = result.body.results[0];
     const location = new Location(query, firstResult);
-    client.query('INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4)', [location.search_query, location.formatted_query, location.latitude, location.longitude]);
+    client.query(SQL.insertLocation, [location.search_query, location.formatted_query, location.latitude, location.longitude]);
     client.query('SELECT * FROM locations;').then(result => {
       console.log(result.rows);
       response.send(location);
     });
+  });
+}
+
+function weatherQuery(request, response) {
+  console.log('weather');
+  const route = 'weather';
+  const query = request.query.data;
+  let locationId;
+  client.query(SQL.getLocationReference, [query.latitude, query.longitude]).then(result => {
+    locationId = result.rows[0].id;
+    client.query('SELECT * FROM weathers WHERE location_id=$1', [locationId]).then(result => {
+      if (result.rows.length) {
+        console.log('from database');
+        console.log(result.rows.map(dayObj => new DailyWeather(dayObj.forecast, dayObj.time)));
+        response.send(result.rows.map(dayObj => new DailyWeather(dayObj.forecast, dayObj.time)));
+      } else {
+        console.log('from internet');
+        searchWeather(query, locationId, response);
+      }
+    });
+  });
+}
+
+function searchWeather(query, locationId, response) {
+  const weatherData = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${query.latitude},${query.longitude}`;
+
+  superagent.get(weatherData).then(result => {
+    const weeklyWeatherArray = result.body.daily.data.map(dayObj => {
+      client.query('INSERT INTO weathers (forecast, time, location_id) VALUES ($1, $2, $3)', [dayObj.summary, dayObj.time, locationId]);
+      return new DailyWeather(dayObj.summary, dayObj.time);
+    });
+    response.send(weeklyWeatherArray);
   });
 }
 
