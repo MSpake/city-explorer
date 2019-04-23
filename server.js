e2318c64013112b5522bd5f436d5efa38a059815
mspake <morgana.spake@gmail.com>
2019-04-22 22:48:02 -0700

    accessing movies api data and rendering to page

--- server.js
+++ server.js
@@ -52,9 +52,20 @@ function Location(query, data) {
   this.longitude = data.geometry.location.lng;
 }
 
-function DailyWeather(forecast, time) {
-  this.forecast = forecast;
-  this.time = new Date(time * 1000).toString().slice(0, 15);
+function DailyWeather(data) {
+  this.forecast = data.summary;
+  this.time = new Date(data.time * 1000).toString().slice(0, 15);
+  // this.created = Date.now()
+}
+
+function Movie(data) {
+  this.title = data.title,
+  this.overview = data.overview,
+  this.average_votes = data.vote_average,
+  this.total_votes = data.vote_count,
+  // this.image_url = data.poster_path,
+  this.popularity = data.popularity,
+  this.released_on = data.release_date;
 }
 
 
@@ -82,6 +93,7 @@ function searchLocation(query, response) {
   const geocodeData = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
 
   superagent.get(geocodeData).then(result => {
+    console.log('location get');
     const firstResult = result.body.results[0];
     const location = new Location(query, firstResult);
     client.query(SQL.insertLocation, [location.search_query, location.formatted_query, location.latitude, location.longitude]);
@@ -94,27 +106,23 @@ function searchLocation(query, response) {
 
 function weatherQuery(request, response) {
   console.log('weather');
-  const route = 'weather';
   const query = request.query.data;
-  let locationId;
-  client.query(SQL.getLocationReference, [query.latitude, query.longitude]).then(result => {
-    locationId = result.rows[0].id;
-    client.query('SELECT * FROM weathers WHERE location_id=$1', [locationId]).then(result => {
-      if (result.rows.length) {
-        console.log('from database');
-        console.log(result.rows);
-        const difference = (Date.now() / 1000) - (parseInt(result.rows[0].time, 10));
-        console.log(difference);
-        if (difference < 86400) {
-          response.send(result.rows.map(dayObj => new DailyWeather(dayObj.forecast, dayObj.time)));
-        } else {
-          searchWeather(query, locationId, response, true);
-        }
+  const route = 'weathers';
+  checkDatabase(route, query).then(result => {
+    if (result) {
+      console.log('from database');
+      console.log(result.rows);
+      const difference = (Date.now() / 1000) - (parseInt(result.rows[0].time, 10));
+      console.log(difference);
+      if (difference < 86400) {
+        response.send(result.rows.map(dayObj => new DailyWeather(dayObj.forecast, dayObj.time)));
       } else {
-        console.log('from internet');
-        searchWeather(query, locationId, response, false);
+        searchWeather(query, result.rows[0].location_id, response, true);
       }
-    });
+    } else {
+      console.log('from internet');
+      searchWeather(query, result.rows[0].location_id, response, false);
+    }
   });
 }
 
@@ -128,10 +136,65 @@ function searchWeather(query, locationId, response, update) {
       } else {
         client.query('INSERT INTO weathers (forecast, time, location_id) VALUES ($1, $2, $3)', [dayObj.summary, dayObj.time, locationId]);
       }
-      return new DailyWeather(dayObj.summary, dayObj.time);
+      return new DailyWeather(dayObj);
     });
     response.send(weeklyWeatherArray);
   });
+}
+
+function moviesQuery(request, response) {
+  const query = request.query.data;
+  const route = 'movies';
+
+  // checkDatabase(route, query).then(result => {
+  //   if (result) {
+  //     console.log('from database');
+  //     console.log(result.rows);
+  //     if () {
+  //     } else {
+  //     }
+  //   } else {
+  //   }
+  // });
+
+  const movieData = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${request.query.data.search_query}`;
+
+  superagent.get(movieData).then(result => {
+    const movieArray = [];
+    for (let i = 0; i < 20; i++) {
+      movieArray.push(new Movie(result.body.results[i]));
+    }
+    response.send(movieArray);
+  });
+}
+
+function yelpQuery(request, response) {
+  const query = request.query.data;
+
+  //
+}
+
+function trailsQuery(request, response) {
+  const query = request.query.data;
+
+  //
+}
+
+function checkDatabase(route, query) {
+  let check = client.query(SQL.getLocationReference, [query.latitude, query.longitude]).then(result => {
+    const locationId = result.rows[0].id;
+    const inDatabase = client.query(`SELECT * FROM ${route} WHERE location_id=$1`, [locationId]).then(result => {
+      if (result.rows.length) return result;
+      else return false;
+    });
+    return inDatabase;
+  });
+  return check;
+}
+
+function handleError(error, response) {
+  console.log(error);
+  if (response) response.status(500).send('Something has gone wrong, unable to complete yor request');
 }
 
 //==========================================
@@ -141,6 +204,13 @@ function searchWeather(query, locationId, response, update) {
 app.get('/location', locationQuery);
 
 app.get('/weather', weatherQuery);
+
+app.get('/movies', moviesQuery);
+
+app.get('/yelp', yelpQuery);
+
+app.get('/trails', trailsQuery);
+
 
 
 // Standard response for when a route that does not exist is accessed.
